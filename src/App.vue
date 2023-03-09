205772ba2b9848aa8c18679691540d35eb5552ca
Leo <e-leokle@iskule.no>
2023-03-09 17:55:26 +0100

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <main class="loginforum">
+    <div>
+      <input id="textEmail" type="email">
+      <label>Email</label>
+    </div>
+    <div>
+      <input id="txtpassword" type="password">
+      <label>password</label>
+    </div>
+    <div>
+      <button id="login">Log in</button>
+    </div>
+    <div>
+      <button id="signup">Sign up</button>
+    </div>
+    
+  </main>
+</template>
+
+<style scoped>
+.loginforum {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f5f5f5;
+}
+</style>
